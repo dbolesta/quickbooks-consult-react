a43aeea6da4b7c22b5ce0a20d8af705e4786a5e5
Damon Bolesta <dbolesta@gmail.com>
2019-09-26 12:45:33 -0400

    adds simple netlify form for testing

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -47,6 +47,50 @@ const Contact = props => {
           or (603) 279-0407 to discuss your QuickBooks® bookkeeping
           needs.
         </p>
+
+        <form role="form" method="post" data-netlify="true">
+          <div class="form-group">
+            <label for="name"> Name:</label>
+            <input
+              type="text"
+              class="form-control"
+              id="name"
+              name="name"
+              required
+              maxlength="50"
+            />
+          </div>
+          <div class="form-group">
+            <label for="email"> Email:</label>
+            <input
+              type="email"
+              class="form-control"
+              id="email"
+              name="email"
+              required
+              maxlength="50"
+            />
+          </div>
+          <div class="form-group">
+            <label for="name"> Message:</label>
+            <textarea
+              class="form-control"
+              type="textarea"
+              name="message"
+              id="message"
+              placeholder="Your Message Here"
+              maxlength="6000"
+              rows="7"
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            class="btn btn-lg btn-success pull-right"
+            id="btnContactUs"
+          >
+            Post It! &rarr;
+          </button>
+        </form>
       </Content>
     </ContentContainer>
   );
